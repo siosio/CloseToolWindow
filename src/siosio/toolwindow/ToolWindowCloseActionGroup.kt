a80dbb4dd87a66dd823023dabf777b0485eb27ae
siosio <siosio12@gmail.com>
2012-07-01 20:24:30 +0900

    メニューの位置を変更。

--- src/siosio/toolwindow/ToolWindowCloseActionGroup.kt
+++ src/siosio/toolwindow/ToolWindowCloseActionGroup.kt
@@ -1,21 +1,29 @@
 package siosio.toolwindow
 
-import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.openapi.wm.ToolWindow
-import com.intellij.openapi.wm.ex.ToolWindowManagerEx
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.wm.ToolWindowAnchor
+import com.intellij.openapi.wm.ex.ToolWindowManagerEx
 
-public class ToolWindowCloseActionGroup() : ActionGroup("Close Tool Window", true) {
+public class ToolWindowCloseActionGroup(): ActionGroup("Close Tool Window", true) {
   public override fun getChildren(p0: AnActionEvent?): Array<AnAction?> {
     return array(
-        ToolWindowCloseAction(ToolWindowAnchor.BOTTOM, "close bottom Tool Windows"),
-        ToolWindowCloseAction(ToolWindowAnchor.RIGHT, "close right Tool Windows"),
-        ToolWindowCloseAction(ToolWindowAnchor.LEFT, "close left Tool Windows")
-        )
+        ToolWindowCloseAction(ToolWindowAnchor.BOTTOM, "Close bottom Tool Window"),
+        ToolWindowCloseAction(ToolWindowAnchor.RIGHT, "Close right Tool Window"),
+        ToolWindowCloseAction(ToolWindowAnchor.LEFT, "Close left Tool Window")
+    )
+  }
+
+  public override fun update(e: AnActionEvent?) {
+    val project = PlatformDataKeys.PROJECT.getData(e!!.getDataContext());
+    val toolWindowManager = ToolWindowManagerEx.getInstanceEx(project)!!
+
+    e!!.getPresentation().setEnabled(
+        toolWindowManager.getToolWindowIds()!!.any {
+          toolWindowManager.getToolWindow(it)!!.isVisible()
+        }
+    )
   }
 }
