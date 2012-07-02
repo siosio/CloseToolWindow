46f4dea515c70cfbb9b91ceae72f36ca93b621f1
siosio <siosio12@gmail.com>
2012-07-02 10:45:22 +0900

    グループをActionGroupで動的に作るのではなく、plugin.xmlで静的に定義するように変更。 これで、それぞれのCloseメニューにショートカットキーを割り当てられるようになる。

--- src/siosio/toolwindow/BaseToolWindowCloseAction.kt
+++ src/siosio/toolwindow/BaseToolWindowCloseAction.kt
@@ -6,7 +6,7 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.wm.ToolWindowAnchor
 import com.intellij.openapi.wm.ex.ToolWindowManagerEx
 
-public class ToolWindowCloseAction(val position: ToolWindowAnchor, val text: String): AnAction(text) {
+public abstract class BaseToolWindowCloseAction(val position: ToolWindowAnchor): AnAction() {
 
   public override fun actionPerformed(e: AnActionEvent?) {
     val project = PlatformDataKeys.PROJECT.getData(e!!.getDataContext());
@@ -22,4 +22,28 @@ public class ToolWindowCloseAction(val position: ToolWindowAnchor, val text: Str
       }
     }
   }
+
+  public override fun update(e: AnActionEvent?) {
+    val project = PlatformDataKeys.PROJECT.getData(e!!.getDataContext());
+    val toolWindowManager = ToolWindowManagerEx.getInstanceEx(project)!!
+
+    e!!.getPresentation().setEnabled(
+        toolWindowManager.getToolWindowIds()!!.any {
+          val toolWindow = toolWindowManager.getToolWindow(it)!!
+          toolWindow.isVisible() && toolWindow.getAnchor() == position
+        }
+    )
+  }
+
 }
+
+public class BottomToolWindowCloseAction(): BaseToolWindowCloseAction(ToolWindowAnchor.BOTTOM) {
+}
+
+public class RightToolWindowCloseAction(): BaseToolWindowCloseAction(ToolWindowAnchor.RIGHT) {
+}
+
+public class LeftToolWindowCloseAction(): BaseToolWindowCloseAction(ToolWindowAnchor.LEFT) {
+}
+
+
